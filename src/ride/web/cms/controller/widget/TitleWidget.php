980b72d907eb24ec6630fecf9cb92d5bdbeaf69e
Joris Vandeweerd <joris@statik.be>
2014-04-01 12:41:18 +0200

    improved StyleWidget implementation

--- src/ride/web/cms/controller/widget/TitleWidget.php
+++ src/ride/web/cms/controller/widget/TitleWidget.php
@@ -5,7 +5,7 @@ namespace ride\web\cms\controller\widget;
 /**
  * Widget to show the name of the current node as title
  */
-class TitleWidget extends AbstractWidget {
+class TitleWidget extends AbstractWidget implements StyleWidget {
 
 	/**
 	 * Machine name of this widget
@@ -36,6 +36,18 @@ class TitleWidget extends AbstractWidget {
     	if ($this->properties->isAutoCache()) {
     	    $this->properties->setCache(true);
     	}
+    }
+
+    /**
+     * Gets the options for the styles
+     * @return array Array with the name of the option as key and the
+     * translation key as value
+     */
+    public function getWidgetStyleOptions() {
+        return array(
+            'container' => 'label.widget.style.container',
+            'title' => 'label.widget.style.title',
+        );
     }
 
 }
